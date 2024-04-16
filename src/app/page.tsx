331483439c8de4a97351892baff972f36bb90c86
AakashJyoti <akjyoti572@gmail.com>
2024-04-16 23:54:36 +0530

    scafold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+const mockUrls = [
+  "https://utfs.io/f/fae5de45-5c6a-4a76-aece-be6af69f5d89-qst08v.png",
+  "https://utfs.io/f/10a99ed9-26c2-4209-856f-a1aaa5140e98-oyw7jz.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image?.id} className="w-48">
+            <img src={image?.url} alt="image" />
+          </div>
+        ))}
+      </div>
       Hello there
     </main>
   );
